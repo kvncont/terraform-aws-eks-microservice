80da9aa4a5d75aadf4a813e058e2e1ddaa3336f9
Kevin Contreras Rodríguez <kvncont@gmail.com>
2025-04-03 04:12:09 +0000

    feat: agregar personalizaciones de VSCode y ajustar configuración de secretos en devcontainer

--- secrets.tf
+++ secrets.tf
@@ -1,4 +1,5 @@
 resource "aws_secretsmanager_secret" "app" {
-  name        = local.app_name
-  description = "Secret for the ${local.app_name} application"
+  name                    = "${local.app_name}/generic"
+  description             = "Secret for the ${local.app_name} application"
+  recovery_window_in_days = 0
 }
